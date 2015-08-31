62fc5664135ec093011ff23eca4c2c8ab8f4186d
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-08-31 18:01:59 +0900

    [add] refs #4 指を認識した時にポインタを表示する

---
+++ LeaPresen/Models/LeapManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+using System.Windows.Media;
+
+namespace LeaPresen.Models
+{
+    public static class LeapManager
+    {
+        private static Controller controller;
+
+        private static Action<double, double> pointAction;
+
+        static LeapManager()
+        {
+            controller = new Controller();
+        }
+
+        public static void Start()
+        { 
+            CompositionTarget.Rendering += Update;
+        }
+
+        public static void SetPointAction(Action<double, double> _pointAction)
+        {
+            pointAction = _pointAction;
+        }
+
+        private static void Update(object sender, EventArgs e)
+        {
+            DrawPoint(controller.Frame());
+        }
+
+        private static void DrawPoint(Frame frame)
+        {
+            var pointable = frame.Pointables.Extended().Leftmost;
+            var box = frame.InteractionBox;
+            var normalizedPosition = box.NormalizePoint(pointable.StabilizedTipPosition);
+            pointAction(normalizedPosition.x * 940, (1 - normalizedPosition.y) * 680);
+        }
+    }
+}
--- LeaPresen/ViewModels/SlideShowViewModel.cs
+++ LeaPresen/ViewModels/SlideShowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using LeaPresen.Models;
 
 namespace LeaPresen.ViewModels
 {
@@ -6,6 +7,45 @@ namespace LeaPresen.ViewModels
     {
         public SlideShowViewModel()
         {
+            LeapManager.SetPointAction(PointAction);
+            LeapManager.Start();
         }
+
+        private void PointAction(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        private double _X;
+
+        public double X
+        {
+            get { return _X; }
+            set
+            {
+                if (_X != value)
+                {
+                    _X = value;
+                    RaisePropertyChanged("X");
+                }
+            }
+        }
+
+        private double _Y;
+
+        public double Y
+        {
+            get { return _Y; }
+            set
+            {
+                if (_Y != value)
+                {
+                    _Y = value;
+                    RaisePropertyChanged("Y");
+                }
+            }
+        }
+
     }
 }
